84e468270dd817a1e725263b8d6e102bb0103bfb
Wellington Felix <wfmf@cin.ufpe.br>
2019-07-01 22:09:11 -0300

    Add redis connection configuration

--- payment-service/src/main/java/br/ufpe/cin/PaymentServiceApplication.java
+++ payment-service/src/main/java/br/ufpe/cin/PaymentServiceApplication.java
@@ -1,23 +1,30 @@
 package br.ufpe.cin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 public class PaymentServiceApplication {
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private Integer redisPort;
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 
     @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory(){
-        return new LettuceConnectionFactory();
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
     }
 
     @Bean
