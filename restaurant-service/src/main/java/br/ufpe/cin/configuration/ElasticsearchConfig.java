f946fb6f30c522328bd9814857c7f59be9267652
Wellington Felix <wfmf@cin.ufpe.br>
2019-07-03 09:07:50 -0300

    Chane ELS config

--- restaurant-service/src/main/java/br/ufpe/cin/configuration/ElasticsearchConfig.java
+++ restaurant-service/src/main/java/br/ufpe/cin/configuration/ElasticsearchConfig.java
@@ -12,24 +12,24 @@ import org.springframework.context.annotation.Configuration;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-@Configuration
+//@Configuration
 public class ElasticsearchConfig {
 
-    @Value("${elasticsearch.host}")
-    private String elasticHost;
-    @Value("${elasticsearch.port}")
-    private Integer elasticPort;
-
-    @Bean
-    public Client client() throws UnknownHostException {
-        Settings settings = Settings.builder()
-                .put("cluster.name", "single-cluster")
-//                .put("node.name", "node1")
-                .put("client.transport.sniff", false)
-                .build();
-        TransportClient transportClient = new PreBuiltTransportClient(settings);
-        transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName(elasticHost), elasticPort));
-        return transportClient;
-    }
+//    @Value("${elasticsearch.host}")
+//    private String elasticHost;
+//    @Value("${elasticsearch.port}")
+//    private Integer elasticPort;
+//
+//    @Bean
+//    public Client client() throws UnknownHostException {
+//        Settings settings = Settings.builder()
+//                .put("cluster.name", "single-cluster")
+////                .put("node.name", "node1")
+//                .put("client.transport.sniff", false)
+//                .build();
+//        TransportClient transportClient = new PreBuiltTransportClient(settings);
+//        transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName(elasticHost), elasticPort));
+//        return transportClient;
+//    }
 
 }
