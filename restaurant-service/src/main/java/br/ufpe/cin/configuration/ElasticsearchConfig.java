3411667c6c2333d8e7b3a22cdcfa63a9243afbd1
Wellington Felix <wfmf@cin.ufpe.br>
2019-07-03 00:52:00 -0300

    Remove node name from client

--- restaurant-service/src/main/java/br/ufpe/cin/configuration/ElasticsearchConfig.java
+++ restaurant-service/src/main/java/br/ufpe/cin/configuration/ElasticsearchConfig.java
@@ -24,7 +24,7 @@ public class ElasticsearchConfig {
     public Client client() throws UnknownHostException {
         Settings settings = Settings.builder()
                 .put("cluster.name", "single-cluster")
-                .put("node.name", "node1")
+//                .put("node.name", "node1")
                 .put("client.transport.sniff", true)
                 .build();
         TransportClient transportClient = new PreBuiltTransportClient(settings);
